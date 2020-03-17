0d3641c1e0981f6897f7abe3559146a7c37e9e92
Valery Kobzar <kobzarvs@gmail.com>
2020-03-17 03:28:55 +0300

    create-react-app + antd

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const {override, fixBabelImports, addWebpackPlugin} = require('customize-cra');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+  addWebpackPlugin(new AntdDayjsWebpackPlugin())
+);
